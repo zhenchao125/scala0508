fae79b7226e02cd3d6134480c2cb9d7fe2f52b97
John Doe <30752740@qq.com>
2019-09-07 16:13:37 +0800

    更新

---
+++ src/main/scala/com/atguigu/scala0508/day04/high/ScanDemo1.scala
@@ -0,0 +1,14 @@
+package com.atguigu.scala0508.day04.high
+
+/**
+  * Author lzc
+  * Date 2019-09-07 16:09
+  */
+object ScanDemo1 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
+/*
+
+ */
