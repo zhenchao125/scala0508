7154d97412d395bf38d291542ccdc6442768ffdc
John Doe <30752740@qq.com>
2019-09-09 08:25:44 +0800

    更新

---
+++ src/main/scala/com/atguigu/scala0508/day05/homework/Homework1.scala
@@ -0,0 +1,31 @@
+package com.atguigu.scala0508.day05.homework
+
+/**
+  * Author lzc
+  * Date 2019-09-09 08:12
+  */
+object Homework1 {
+    def main(args: Array[String]): Unit = {
+        
+    }
+    
+    
+}
+
+
+/*
+1. 使用 reduce 计算集合中的最大值
+
+2. 使用 foldLeft 同时计算最大值和最小值
+
+3. 编写一个函数, 接收一个字符串, 返回一个 Map.
+    比如: indexes("Helloee")
+
+   返回: Map(H->{0}, e -> {1, 5, 6}, ...)   数字其实是下标
+
+4. 实现一个函数，作用与mkString相同，使用foldLeft
+
+5. 实现一个 scala 版的 wordcount
+   , 然后读取文件实现
+   Source.
+ */
